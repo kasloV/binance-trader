841e34fedceb1caa95998d6386f7742501ff0925
Artur Gawron <arturgawron6@gmail.com>
2022-07-24 00:42:03 +0200

    Prepare app for production run

--- kafka/topics.sh
+++ kafka/topics.sh
@@ -1,4 +1,4 @@
 # /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server='kafka:9092' --list
 
-( sleep 5; /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server='kafka:9092' --if-not-exists --create --topic='book' ) &
-( sleep 5; /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server='kafka:9092' --if-not-exists --create --topic='trades' ) &
+( sleep 5; /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server=kafka:9092 --create --topic=book --config retention.ms=10000 --config segment.ms=10000 ) &
+( sleep 5; /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server=kafka:9092 --create --topic=trades --config retention.ms=2400000 --config segment.ms=10000 ) &
